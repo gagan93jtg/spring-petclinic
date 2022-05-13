0159c549058c704902f2792ebecbd5ba9ce200e7
Gagandeep Singh <gagandeep@babatrucks.com>
2022-05-13 13:30:27 +0530

    Add minimalistic POJOs.

---
+++ src/main/java/com/sprintpetclinic/petclinic/models/Owner.java
@@ -0,0 +1,4 @@
+package com.sprintpetclinic.petclinic.models;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/com/sprintpetclinic/petclinic/models/Vet.java
@@ -0,0 +1,4 @@
+package com.sprintpetclinic.petclinic.models;
+
+public class Vet extends Person{
+}
